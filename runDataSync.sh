83bbfe2f676cecdb2ec72336b95efd245af70a81
Renien <renien.john@gmail.com>
2017-04-28 11:50:32 +0530

    Modified to parameter expansion and added condition for cloud type

--- runDataSync.sh
+++ runDataSync.sh
@@ -10,17 +10,29 @@ done
 
 echo "Done parsing args..."
 
-# Default values
-scripts_root="../modules"
-
 # Check the OS type and load the date utils
 OS_TYPE=$(uname -s)
 
 if [ $OS_TYPE == "Linux" ]; then
 	echo "It's a linux machine.."
-
+	source "$scripts_root/dateUtilsLinux.sh"
 elif [ $OS_TYPE == "Darwin" ]; then
 	echo "It's a Mac.."
-	source scripts_root/dateUtilsMac.sh
+	source "${scripts_root}/dateUtilsMac.sh"
 fi
+
+# Load the authendication based cloud service 
+if [ $CLOUD_TYPE == "AWS" ]; then
+	echo "It's for AWS S3 bucket.."
+	
+	source "${scripts_root}/authentication.sh"
+	echo "AWS_CONF_FILE path :" $AWS_CONF_FILE 
+	aws_authentication $AWS_CONF_FILE
+
+elif [ $CLOUD_TYPE == "GCP" ]; then
+	echo "It's for GCP Storage.."
+fi
+
+# Run the incremental copy/batch copy 
+
 
