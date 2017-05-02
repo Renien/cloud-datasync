84a4384cb7131dffb2df8d7c12aebe30d299527f
Renien <renien.john@gmail.com>
2017-05-02 13:34:04 +0530

    added more sample command with additional parameter

--- sample_AWS_command.sh
+++ sample_AWS_command.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+# Parameter with startDate and endDate
+# ./runDataSync.sh OS_TYPE=Mac \
+# 	CLOUD_TYPE=AWS \
+# 	AWS_CONF_FILE=/Users/renienj/open-source-projects/cloud-datasync/modules/conf/aws-conf.properties \
+# 	scripts_root=/Users/renienj/open-source-projects/cloud-datasync/modules \
+# 	IS_INCREMENTAL=true \
+# 	startDate=2017-01-01 \
+# 	endDate=2017-04-28 \
+# 	SOURCE=/your-source-dir/ \
+# 	TARGET=s3://aws-s3-bucket-dir
+
+# Parameter with period
 ./runDataSync.sh OS_TYPE=Mac \
 	CLOUD_TYPE=AWS \
 	AWS_CONF_FILE=/Users/renienj/open-source-projects/cloud-datasync/modules/conf/aws-conf.properties \
 	scripts_root=/Users/renienj/open-source-projects/cloud-datasync/modules \
 	IS_INCREMENTAL=true \
-	startDate=2017-01-01 \
-	endDate=2017-04-28
+	period=90 \
+	SOURCE=/your-source-dir/ \
+	TARGET=s3://aws-s3-bucket-dir/
+
+# Parameter for bulk upload
+# ./runDataSync.sh OS_TYPE=Mac \
+# 	CLOUD_TYPE=AWS \
+# 	AWS_CONF_FILE=/Users/renienj/open-source-projects/cloud-datasync/modules/conf/aws-conf.properties \
+# 	scripts_root=/Users/renienj/open-source-projects/cloud-datasync/modules \
+# 	IS_INCREMENTAL=false \
+	# SOURCE=/your-source-dir/ \
+	# TARGET=s3://aws-s3-bucket-dir/
