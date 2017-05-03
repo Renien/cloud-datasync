556e4f76ea438a5d6652cae1c685fdbc30bc4269
Renien <renien.john@gmail.com>
2017-05-03 13:45:47 +0530

    Updated the parameters in sample commands

--- sample_AWS_command.sh
+++ sample_AWS_command.sh
@@ -9,7 +9,9 @@
 # 	startDate=2017-01-01 \
 # 	endDate=2017-04-28 \
 # 	SOURCE=/your-source-dir/ \
-# 	TARGET=s3://aws-s3-bucket-dir
+# 	TARGET=s3://aws-s3-bucket-dir \
+#   PARTIONDIRECTORY= \
+#	DATESEPARATOR=/
 
 # Parameter with period
 ./runDataSync.sh OS_TYPE=Mac \
@@ -19,7 +21,9 @@
 	IS_INCREMENTAL=true \
 	period=90 \
 	SOURCE=/your-source-dir/ \
-	TARGET=s3://aws-s3-bucket-dir/
+	TARGET=s3://aws-s3-bucket-dir/ \
+	PARTIONDIRECTORY=date= \
+	DATESEPARATOR=-
 
 # Parameter for bulk upload
 # ./runDataSync.sh OS_TYPE=Mac \
@@ -27,5 +31,6 @@
 # 	AWS_CONF_FILE=/Users/renienj/open-source-projects/cloud-datasync/modules/conf/aws-conf.properties \
 # 	scripts_root=/Users/renienj/open-source-projects/cloud-datasync/modules \
 # 	IS_INCREMENTAL=false \
-	# SOURCE=/your-source-dir/ \
-	# TARGET=s3://aws-s3-bucket-dir/
+#   SOURCE=/your-source-dir/ \
+#   TARGET=s3://aws-s3-bucket-dir/ \
+#   PARTIONDIRECTORY=date
